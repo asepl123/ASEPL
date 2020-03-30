1083171e016d0e00f1de57a26d5a0b3269d067dd
Tarang Kalaria <41413595+tarang1606@users.noreply.github.com>
2020-03-30 15:35:20 +0530

    Add files via upload

---
+++ OpenTap.Plugins.PnaPlugin/OpenTap.Plugins.PnaPlugin/Instrument/KeysightPna.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Forms;
+using OpenTap;  // Use OpenTAP infrastructure/core components (log,TestStep definition, etc)
+
+namespace OpenTap.Plugins.PnaPlugin
+{
+	[Display("PNA", Groups: new[] { "SAC" }, Description: "A Keysight PNA Instrument")]
+	public class KeysightPna : ScpiInstrument
+	{
+
+        public KeysightPna()
+		{
+            Name = "PNA N5224A";
+            VisaAddress = "Simulate";
+		}
+
+        public override void Open()
+        {
+            // This will be called at the beginning of TestPlan execution.
+            base.Open();
+
+            // Instruments also have access to a Log object.
+            MessageBox.Show($"Connection to {VisaAddress} successful");
+            Log.Info(string.Format("Connection to {0} successful", VisaAddress));
+
+        }
+
+        // Close procedure for the instrument.
+        public override void Close()
+        {
+            // Add code needed for closing the resource here.
+            // This will be called at the end of TestPlan execution.
+            base.Close();
+        }
+
+        // Public methods can be called from Test Steps.
+        public void SetInputData(double[] inputData)
+        {
+            // This would be some instrument control code to write the input data to generator.
+
+            // ScpiCommand("INPut {0}", inputData);
+        }
+
+    }
+}
---
+++ OpenTap.Plugins.PnaPlugin/OpenTap.Plugins.PnaPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenTap.Plugins.PnaPlugin")]
+[assembly: AssemblyDescription("PNA Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Automation Systems Engineer Pvt Ltd")]
+[assembly: AssemblyProduct("OpenTap.Plugins.PnaPlugin")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e9a58818-52f6-4e3a-ade2-22041cf7626b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// If you are using GIT and git tags, this project is set up to automatically update 
+// these values when building in release mode.
+// To add a tag use 'git tag -a v1.0 -m "version 1.0"' from the command line.
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-IN")]
